f02cbf4015c5f08951776b25111243ae21e098b9
Richard Jie Cui <richard.cui@utoronto.ca>
2019-05-26 16:50:00 -0500

    change version info

--- eegplugin_mefimport.m
+++ eegplugin_mefimport.m
@@ -32,7 +32,7 @@ function vers = eegplugin_mefimport(fig, try_strings, catch_strings)
 
 % version info
 % ------------
-vers='MEF_import0.5';
+vers='MEF_import0.8';
 
 % parse inputs
 % ------------
