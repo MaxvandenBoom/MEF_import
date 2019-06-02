a899adedf9f677a36a3d71d645fd8b7862fc1365
Richard Jie Cui <richard.cui@utoronto.ca>
2019-06-02 13:56:28 -0500

    improve MEF matlab class

--- eegplugin_mefimport.m
+++ eegplugin_mefimport.m
@@ -23,7 +23,7 @@ function vers = eegplugin_mefimport(fig, try_strings, catch_strings)
 % See also .
 
 % Copyright 2019 Richard J. Cui. Created: Sun 04/28/2019  9:51:01.691 PM
-% $Revision: 0.8 $  $Date: Wed 05/29/2019 10:16:53.998 PM$
+% $Revision: 0.9 $  $Date: Sun 06/02/2019  1:55:35.201 PM $
 %
 % 1026 Rocky Creek Dr NE
 % Rochester, MN 55906, USA
@@ -32,7 +32,7 @@ function vers = eegplugin_mefimport(fig, try_strings, catch_strings)
 
 % version info
 % ------------
-vers='MEF_import1.0';
+vers='MEF_import1.05';
 
 % parse inputs
 % ------------
--- mef_matlab/@MultiscaleElectrophysiologyFile/SampleIndex2Time.m
+++ mef_matlab/@MultiscaleElectrophysiologyFile/SampleIndex2Time.m
@@ -24,7 +24,7 @@ function [sample_time, sample_yn] = SampleIndex2Time(this, varargin)
 % See also SampleTime2Index.
 
 % Copyright 2019 Richard J. Cui. Created: Mon 05/06/2019  9:29:08.940 PM
-% $Revision: 0.5 $  $Date: Thu 05/30/2019 11:22:03.407 PM$
+% $Revision: 0.7 $  $Date: Sun 06/02/2019 12:00:04.051 AM$
 %
 % 1026 Rocky Creek Dr NE
 % Rochester, MN 55906, USA
@@ -39,8 +39,6 @@ st_unit = q.st_unit;
 
 % set paras
 % ----------
-fs = this.Header.sampling_frequency;
-MPS = 1e6; % microsecond per second
 sample_time = zeros(size(sample_index));
 sample_yn = false(size(sample_index));
 [sorted_si, orig_index] = sort(sample_index);
@@ -53,65 +51,32 @@ cont = this.Continuity;
 % within continuous segment
 % -------------------------
 cont_start_end = cont{:, {'SampleIndexStart', 'SampleIndexEnd'}};
-% sorted_sample_time = sample_index;
-% sorted_sample_yn = sample_yn;
-% num_seg = size(cont_start_end, 1); % number of segments
-% for k = 1:num_seg
-%     start_k = cont_start_end(k, 1);
-%     end_k = cont_start_end(k, 2);
-%     ind_k = sorted_si >= start_k & sorted_si <= end_k;
-%     
-%     if sum(ind_k) ~= 0
-%         st_k = cont.SampleTimeStart(k);
-%         index_diff = sorted_si(ind_k)-start_k;
-%         time_diff = index_diff*MPS/fs;
-%         sorted_st_k = st_k+time_diff;
-%         sorted_sample_time(ind_k) = round(sorted_st_k); % uUTC integer
-%         sorted_sample_yn(ind_k) = true;
-%     end % if
-% end % for
 
 % choose continuity segment that in the range of sample indexes
 if numel(sorted_si) == 1
-    sel_cont_ind = sorted_si <= cont_start_end(:, 2) ...
-        & sorted_si >= cont_start_end(:, 1);
-else
-    sel_cont_ind = sorted_si(1) <= cont_start_end(:, 2) ...
-        & sorted_si(2) >= cont_start_end(:, 1);
+    sorted_si = sorted_si*[1 1];
 end % if
+sel_cont_ind = sorted_si(1) <= cont_start_end(:, 2) ...
+    & sorted_si(end) >= cont_start_end(:, 1);
 sel_cont = cont(sel_cont_ind, :); % select the segment of continuity in the
                                   % range of sorted_si
 sel_cont_start_end = cont_start_end(sel_cont_ind, :);                                  
 [sorted_sample_time, sorted_sample_yn] = inContLoopCont(sel_cont_start_end,...
-    sel_cont, sorted_si, MPS, fs);
+    sel_cont, sorted_si);
 
 % within discontinous segment
 % ----------------------------
 a = cont_start_end.';
 b = cat(1, -inf, a(:), inf);
 discont_start_end = reshape(b, 2, numel(b)/2).';
-num_seg = size(discont_start_end, 1); % number of segments
-for k = 1:num_seg
-    start_k = discont_start_end(k, 1);
-    end_k = discont_start_end(k, 2);
-    ind_k = sorted_si > start_k & sorted_si < end_k;
-    
-    if sum(ind_k) ~= 0
-        if start_k == -inf
-            st_k = cont.SampleTimeStart(k);
-            index_diff = sorted_si(ind_k)-end_k;
-            time_diff = index_diff*MPS/fs;
-            sorted_st_k = st_k+time_diff;
-        else
-            st_k = cont.SampleTimeEnd(k-1);
-            index_diff = sorted_si(ind_k) - start_k;
-            time_diff = (index_diff-1)*MPS/fs;
-            sorted_st_k = st_k+time_diff+1;
-        end % if
-        sorted_sample_time(ind_k) = round(sorted_st_k); % uUTC integer
-        sorted_sample_yn(ind_k) = false;
-    end % if
-end % for
+not_discont_index = discont_start_end(:, 2)-discont_start_end(:, 1) == 1;
+discont_start_end(not_discont_index, :) = [];
+discont_index = ~not_discont_index;
+sel_cont_ind = [true; discont_index(2:end-2); true];
+sel_cont = cont(sel_cont_ind, :);
+[sorted_sample_time, sorted_sample_yn] = inDiscontLoopDiscont(...
+    sorted_sample_time, sorted_sample_yn, discont_start_end, sel_cont,...
+    sorted_si);
 
 % output
 % ------
@@ -135,10 +100,69 @@ end
 % =========================================================================
 % subroutines
 % =========================================================================
-function [s_ind, s_yn] = inContLoopCont(cont_se, cont, sorted_si, MPS, fs)
+function [s_time, s_yn] = inContLoopCont(cont_se, cont, sorted_si)
+
+blk_len = 2^20; % length of one block of sample indexed
+num_si = numel(sorted_si);
+if num_si >= 3*blk_len % set verbose
+    verb = true;
+else
+    verb = false;
+end % if
+
+s_time = zeros(size(sorted_si));
+s_yn = false(size(sorted_si));
+
+num_blk = ceil(num_si/blk_len);
+if verb, wh = waitbar(0, 'Coverting sample indexes to time...'); end % if
+for k = 1:num_blk
+    start_k = (k-1)*blk_len+1;
+    end_k = k*blk_len;
+    if end_k >= num_si
+        end_k = num_si;
+    end % if
+    si_k = sorted_si(start_k:end_k);
+    [s_time_k, s_yn_k] = inContLoopCont_blk(cont_se, cont, si_k);
+    
+    s_time(start_k:end_k) = s_time_k;
+    s_yn(start_k:end_k) = s_yn_k;
+    if verb, waitbar(k/num_blk, wh), end % if
+end % for
+if verb, close(wh), end % if
+
+end % function
+
+function [s_time, s_yn] = inDiscontLoopDiscont(s_time, s_yn, discont_se,...
+    cont, sorted_si)
+
+num_seg = size(discont_se, 1); % number of segments
+for k = 1:num_seg
+    start_k = discont_se(k, 1);
+    end_k = discont_se(k, 2);
+    ind_k = sorted_si > start_k & sorted_si < end_k;
+    
+    if sum(ind_k) ~= 0
+        if start_k == -inf
+            st_k = cont.SampleTimeStart(k);
+            index_diff = sorted_si(ind_k)-end_k;
+            time_diff = index_diff*MPS/fs;
+            sorted_st_k = st_k+time_diff;
+        else
+            st_k = cont.SampleTimeEnd(k-1);
+            index_diff = sorted_si(ind_k) - start_k;
+            time_diff = (index_diff-1)*MPS/fs;
+            sorted_st_k = st_k+time_diff+1;
+        end % if
+        s_time(ind_k) = round(sorted_st_k); % uUTC integer
+        s_yn(ind_k) = false;
+    end % if
+end % for
+end % function
+
+function [s_time, s_yn] = inContLoopCont_blk(cont_se, cont, sorted_si)
 % within continuous segment loop through continuity segments
 
-s_ind = zeros(size(sorted_si));
+s_time = zeros(size(sorted_si));
 s_yn = false(size(sorted_si));
 
 num_seg = size(cont_se, 1); % number of segments
@@ -149,10 +173,11 @@ for k = 1:num_seg
     
     if sum(ind_k) ~= 0
         st_k = cont.SampleTimeStart(k);
+        et_k = cont.SampleTimeEnd(k);
+        slop_k = (et_k-st_k)/(end_k-start_k);
         index_diff = sorted_si(ind_k)-start_k;
-        time_diff = index_diff*MPS/fs;
-        sorted_st_k = st_k+time_diff;
-        s_ind(ind_k) = round(sorted_st_k); % uUTC integer
+        sorted_st_k = st_k+slop_k*index_diff; % use linear approximation
+        s_time(ind_k) = round(sorted_st_k); % uUTC integer
         s_yn(ind_k) = true;
     end % if
 end % for
