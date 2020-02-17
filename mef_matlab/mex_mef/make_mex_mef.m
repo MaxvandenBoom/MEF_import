2f519cb4096b66e3822f2121a211137727fd4a76
Richard Jie Cui <richard.cui@utoronto.ca>
2020-02-16 21:54:48 -0600

    move content of meflib to folder matmef

--- mef_matlab/mex_mef/make_mex_mef.m
+++ mef_matlab/mex_mef/make_mex_mef.m
@@ -1,7 +1,7 @@
 % Compile mex files required to process MEF files
 
 % Copyright 2019-2020 Richard J. Cui. Created: Wed 05/29/2019  9:49:29.694 PM
-% $Revision: 0.5 $  $Date: Sun 02/02/2020  8:59:16.389 PM $
+% $Revision: 0.6 $  $Date: Sun 02/16/2020  9:47:59.284 PM $
 %
 % 1026 Rocky Creek Dr NE
 % Rochester, MN 55906, USA
@@ -41,16 +41,24 @@ fprintf('===== Processing MEF 3.0 format =====\n')
 
 cd([p, '/mef_3p0'])
 fprintf('Building read_mef_header_mex_3p0.mex*\n')
-mex -output read_mef_header_mex_3p0 read_mef_header_mex_3p0.c matmef/meflib/meflib/meflib.c matmef/meflib/meflib/mefrec.c matmef/matmef_mapping.c matmef/mex_datahelper.c
+mex -output read_mef_header_mex_3p0 ...
+    read_mef_header_mex_3p0.c ...
+    matmef/meflib/meflib/meflib.c ...
+    matmef/meflib/meflib/mefrec.c ...
+    matmef/matmef_mapping.c ...
+    matmef/mex_datahelper.c
 
 cd([p, '/mef_3p0/matmef/']) % assume 'mef_3p0' is the subdirectory
 fprintf('\n')
 fprintf('Building read_mef_session_metadata.mex*\n')
-mex -output read_mef_session_metadata read_mef_session_metadata.c meflib/meflib/meflib.c meflib/meflib/mefrec.c matmef_mapping.c mex_datahelper.c
+mex -output read_mef_session_metadata read_mef_session_metadata.c ...
+    meflib/meflib/meflib.c meflib/meflib/mefrec.c matmef_mapping.c ...
+    mex_datahelper.c
 
 fprintf('\n')
 fprintf('Building read_mef_ts_data.mex*\n')
-mex -output read_mef_ts_data read_mef_ts_data.c matmef_data.c meflib/meflib/meflib.c meflib/meflib/mefrec.c
+mex -output read_mef_ts_data read_mef_ts_data.c matmef_data.c ...
+    meflib/meflib/meflib.c meflib/meflib/mefrec.c
 
 cd(cur_dir)
 
