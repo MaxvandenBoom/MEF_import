71c1713e306cf7fc7ff45db68e2af261eff34850
Richard Jie Cui <richard.cui@utoronto.ca>
2020-02-17 20:32:44 -0600

    attempt to improve helper functions

--- mef_matlab/mex_mef/mef_3p0/matmef/mex_datahelper.h
+++ mef_matlab/mex_mef/mef_3p0/matmef/mex_datahelper.h
@@ -1,3 +1,17 @@
+/**
+ * 	@file
+ * 	Functions to convert primitive c-datatypes to matlab primitive (1x1) arrays/matrices
+ *	
+ *  Copyright 2020, Max van den Boom
+ *
+ *  
+ *  This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // Copyright 2020 Richard J. Cui. Created: Sun 02/16/2020 10:34:49.777 PM
 // $Revision: 0.2 $  $Date: Mon 02/17/2020  8:15:44.891 PM $
 //
@@ -18,12 +32,12 @@
 #include "meflib.h"
 
 mxArray *mxFillNumericArray(ui1 *array, int num_bytes);
-mxArray *mxDoubleByValue(sf8 value);
-mxArray *mxUInt32ByValue(ui4 value);
+mxArray *mxStringByUTF8Value(char *str);
+        
 mxArray *mxInt8ByValue(si1 value);
+mxArray *mxUInt32ByValue(ui4 value);
 mxArray *mxInt32ByValue(si4 value);
 mxArray *mxInt64ByValue(si8 value);
-mxArray *mxStringByUTF8Value(char *str);
-
+mxArray *mxDoubleByValue(sf8 value);
 
 #endif   // MEX_DATAHELPER_
